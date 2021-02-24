afc3203241d8cfc6d542acae64cf084d7774ac0c
Abujadda <kasimuadamumusa100@gmail.com>
2021-02-24 11:14:52 -0800

    new

--- config/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="navbar navbar-default footer navbar-fixed-bottom">
-		<div class="container">
-			<p class="text-center">Copyright &copy; 2021 - All Rights Reserved.</p>
-		</div>
-	</div>
